719660da15ea1b9089ca19bbe8049c295e2b7e2a
Etienne Cotin <etienne.cotin@gmail.com>
2019-10-23 19:06:12 +0200

    add method for treat api result

---
+++ src/services/TreatFullState.js
@@ -0,0 +1,12 @@
+export const treat = sensorList => {
+  let node = [];
+  node = sensorList.reduce((acc, sensor) => {
+    if (acc[sensor.nativeNodeId]) {
+      acc[sensor.nativeNodeId] = [...acc[sensor.nativeNodeId], sensor];
+    } else {
+      acc[sensor.nativeNodeId] = [sensor];
+    }
+    return acc;
+  }, []);
+  return node;
+};
